e42a16b6b0b0e0e985a03bc73c5d1020dda97ebe
Luke Phoenix <luke13145201201@163.com>
2026-04-07 14:23:38 +0800

    把易和奇门的基础知识写入了数据库。

---
+++ core.py
@@ -0,0 +1,20 @@
+import YiDate
+
+#attributes = dir(YiDate)
+
+#for attribute in attributes:
+#    print('Print YiDate Atttribute : ' , attributes)
+
+
+
+YiIntDict = YiDate.YiIntDict
+print('Print YiIntDict : ' , YiIntDict)
+
+
+print('Print YiIntDict[63] : ' , YiIntDict[63])
+print('Print YiIntDict[0b111111] : ' , YiIntDict[0b111111])
+print('Print Bin 0b111111',bin(0b111111))
+print('Print Bin 63',bin(63))
+
+
+#End Link

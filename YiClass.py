ddea14f93ce8a98813f89d4f47c9e8f79fcbcb17
Luke Phoenix <luke13145201201@163.com>
2026-04-07 20:58:21 +0800

    开始写易类YiJingClass.

---
+++ YiClass.py
@@ -0,0 +1,8 @@
+class YiJingClass:
+    def __init__(self, name, value):
+        self.name = name
+        self.value = value
+        self.YiBinList = 
+
+    def __str__(self):
+        return f"{self.name}: {self.value}"

d4eb00ccde4f1e3e000194a414555f84bd37fee6
Luke Phoenix <luke13145201201@163.com>
2026-04-08 10:17:26 +0800

    YiClass.py 导入了YiDate模块并编写了YiJingClass。

--- YiClass.py
+++ YiClass.py
@@ -1,8 +1,19 @@
+import YiDate
+
+
+
 class YiJingClass:
     def __init__(self, name, value):
+        print(f"Creating {name}")
         self.name = name
         self.value = value
-        self.YiBinList = 
+        self.YI_GUA_BIN_ORDER_LIST = YiDate.YiIntList
 
     def __str__(self):
         return f"{self.name}: {self.value}"
+
+    def __del__(self):
+        print(f"Deleting {self.name}")
+
+    def get_yi_gua_bin_order_list(self):
+        return self.YI_GUA_BIN_ORDER_LIST
